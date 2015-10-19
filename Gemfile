c4aea60d31994d1e71c812123635b1ae4e6210c2
Kenner Kliemann <kenner.hp@gmail.com>
2015-10-19 17:28:26 -0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+ruby '2.2.3'
+
+gem 'github-pages', '~> 39'
+gem 'jekyll-redirect-from', '~> 0.8.0'
+gem 'rake', '~> 10.4'
+gem 'rmagick', '~> 2.15'
+gem 'foreman'
---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    copy: {
+      main: {
+        files: [
+          {
+            expand: true,
+            flatten: true,
+            cwd: 'node_modules/basscss/css/',
+            src: 'basscss.min.css',
+            dest: '_sass/',
+            filter: 'isFile'
+          }
+        ]
+      }
+    },
+    imagemin: {                          // Task
+      dynamic: {                         // Another target
+        files: [{
+          expand: true,
+          cwd: '_images/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'public/images/'
+        }]
+      }
+    }
+  });
+
+  // Load the plugins
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+  // Default task(s).
+  grunt.registerTask('default', ['copy', 'imagemin']);
+};
